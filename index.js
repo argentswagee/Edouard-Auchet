d69b10ca6c0c46c31b79f4efdacec4f57ebd90a5
Edouard Auchet <125192936+argentswagee@users.noreply.github.com>
2024-06-08 01:13:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-if function getRandomInt(min, max) {
+document.getElementById('menu').style.display = 'none';
+var prv = false;
+
+document.getElementById('menulogo').addEventListener('click', function(){
+  if (prv === false) {
+    document.getElementById('menu').style.display = 'block';
+    prv = true;
+  } else {
+    document.getElementById('menu').style.display = 'none';
+    prv = false;
+  }
+})
+
+function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
@@ -19,11 +32,11 @@ function getRandomInt2(min, max) {
   }
 
 document.getElementById('générer').addEventListener('click', function(){
-    const wordList2 = ["baobab", "arbre", "gourmand", "gras", "seum", "caca", "tournevis", "marteau", "canif", "ciseau", "téléphone", "scotch", "tourtel", "infection", "pensement", "chapeau", "canapé", "vélo", "livre", "écran", "singe", "bouffon", "morve", "pupitre", "bonjour", "coucou", "hirondelle", "baka", "sylvain", "dimanche" ];
+    const wordList2 = ["ton baobab", "ton arbre", "ton gourmand", "ton gras", "ton seum", "ton caca", "ton sylvain", "ton baka", "pays", "ton saperlipopette", "ton neuille ", "ta gourmandise", "ton argument", "ton infection", "ton pensement", "ton dimanche", "ta salopette", "ton C15", "ton singe", "ton bouffon", "ta morve", "ton pupitre", "ton bonjour", "ton coucou", "ton hirondelle" ];
     const randomWord2 = getRandomWord2(wordList2);
     console.log("Mot choisi au hasard :", randomWord2);
 
-    const wordList = ["va dormir dans ton ", "ravale ton ", "va manger ton ", "déguste ton ", "reste dans ton ", "retourne dans ton ", "Lis dans ton ", "va courir dans ton ", "va nager dans ton ", "Chante dans ton ", "va jouer dans ton ", "Graille ton ", "témoigne ton "   ];
+    const wordList = ["va dormir dans ", "ravale ", "va manger ", "déguste ", "reste dans ", "retourne dans ", "Lis dans ", "va courir dans ", "va nager dans ", "Chante dans ", "va jouer dans ", "Graille ", "témoigne ", "édifie "];
     const randomWord = getRandomWord(wordList);
     console.log("Mot choisi au hasard :", randomWord);
     document.getElementById('debtxt').textContent = randomWord + randomWord2;
